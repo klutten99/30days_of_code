6ccbfdd1e9deb9cdccbd7bc9a7e3bf2ef45a209d
Wiberg <klutten99@gmail.com>
2020-10-15 23:09:50 +0200

    Days completed

---
+++ day_20.py
@@ -0,0 +1,28 @@
+#Day 20: Sorting
+
+import sys
+
+n = int(input().strip())
+a = list(map(int, input().strip().split(' ')))
+# Write Your Code Here
+numberOfSwaps = 0;
+for i in range(n):
+    # Track number of elements swapped during a single array traversal
+
+    for j in range(n - 1):
+        # Swap adjacent elements if they are in decreasing order
+        if (a[j] > a[j + 1]):
+            # Swap a[j] and a[j + 1]
+            temp = a[j]
+            a[j] = a[j + 1]
+            a[j + 1] = temp
+
+            numberOfSwaps += 1
+
+    # If no elements were swapped during a traversal, array is sorted
+    if (numberOfSwaps == 0):
+        break;
+
+print("Array is sorted in " + str(numberOfSwaps) + " swaps.")
+print("First Element: " + str(a[0]))
+print("Last Element: " + str(a[len(a) - 1]))
